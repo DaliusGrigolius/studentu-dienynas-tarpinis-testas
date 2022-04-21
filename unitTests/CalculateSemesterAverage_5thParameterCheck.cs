58d63af140eabe01bc6fd1201d99db54a8ae401e
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-04-21 21:43:51 +0300

    resolved unitTests

---
+++ unitTests/CalculateSemesterAverage_5thParameterCheck.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Services;
+
+namespace unitTests
+{
+    [TestClass]
+    public class CalculateSemesterAverage_5ndParameterCheck
+    {
+        [TestMethod]
+        public void CalculateSemesterAverage_HasData_ReturnsCorrectlyIf5rdParameterWrong()
+        {
+            Calculator calculator = new Calculator();
+            CalculateSemesterAverage_2ndParameterCheck smt = new CalculateSemesterAverage_2ndParameterCheck();
+            var schoolsList = smt.schoolsList;
+            var actual = calculator.CalculateSemesterAverage(schoolsList, 0, 0, 0, -1);
+            var expected = "";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
